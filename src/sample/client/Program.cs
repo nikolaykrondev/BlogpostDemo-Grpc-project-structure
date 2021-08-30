1ebd322a6c5d0c6620b1957e7265ca8549c9d0fc
Nikolay Krondev <nikolaykrondev@users.noreply.github.com>
2021-08-30 13:44:40 +0200

    Adjust client for demo

--- src/sample/client/Program.cs
+++ src/sample/client/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Krondev.Greeter;
+using Krondev.Greeter.Clients;
+using Krondev.Greeter.Configuration;
 
 namespace client
 {
     class Program
     {
-        static void Main(string[] args)
+        private static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+                // The following statement allows you to call insecure services. To be used only in development environments.
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            }
+
+            var httpHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            await using var client = new GreeterClient(ChannelCredentials.Insecure, new GreeterClientOptions
+            {
+                Host = "http://localhost",
+                Port = 5000,
+                HttpHandler = httpHandler
+            });
+
+            var reply = await client.SayHello(
+                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message + " with status " + reply.Status);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
